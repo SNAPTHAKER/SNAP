629848058fc34b84ad374b5b5f98a1076497feb6
SNAPTHAKER <90932003+SNAPTHAKER@users.noreply.github.com>
2021-09-19 06:08:21 +0300

    Add files via upload

--- SNAP.lua
+++ SNAP.lua
@@ -4454,6 +4454,7 @@ keyboard.inline_keyboard = {
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/iinzzz&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+Dev_Abs(msg.chat_id_, msg.id_, 1, text, 1, 'md')
 end
 --     Source SNAP     --
 if ChatType == 'sp' or ChatType == 'gp'  then
