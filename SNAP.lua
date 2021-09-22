6c5e02d14597caa263611cf5e074f75e48a3af38
SNAPTHAKER <90932003+SNAPTHAKER@users.noreply.github.com>
2021-09-23 00:42:51 +0300

    Add files via upload

--- SNAP.lua
+++ SNAP.lua
@@ -4457,7 +4457,7 @@ keyboard.inline_keyboard = {
 {{text = '⌔ Developer',url="t.me/yk_uu"},{text = '⌔ Tws LaricA',url="https://t.me/sSs_BoT"}},{{text= '⌔ Support Source .',url="https://t.me/joinchat/u_nILa3LVoUyYzI8"}}
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/SNAP&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/SNAPTame&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
 --     Source SNAP     --
